0e44d330e01bd585b925bcec2db8b3cd2f0438ba
Charles Zhang <charliezhang@Junfengs-Air.nc.rr.com>
2016-10-01 12:11:04 -0400

    fill/unfill

--- init.el
+++ init.el
@@ -276,6 +276,20 @@ cursor to the new line."
 
     (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)))
 
+;;; Unfill/fill
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
 (use-package multiple-cursors
   :ensure t
   :bind (("C-M-c" . mc/edit-lines)
