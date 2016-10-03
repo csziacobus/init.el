1df4d41c95e830f0bff2745cce3491f20b3a1466
Charles Zhang <charleszhang99@yahoo.com>
2016-10-03 17:41:10 -0400

    Get rid of gnus stuff

--- init.el
+++ init.el
@@ -83,15 +83,6 @@
           (rename-buffer new-name)
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
-
-;; mail contacts
-(use-package bbdb
-  :ensure t)
-
-;; git gnus
-;; (add-to-list 'load-path "~/.emacs.d/gnus/lisp/")
-;; (require 'gnus-load)
-
 ;; magit
 (use-package magit
   :bind ("C-c v" . magit-status)
